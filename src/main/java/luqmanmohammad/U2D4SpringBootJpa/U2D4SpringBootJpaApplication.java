5cd6756a0b03b12b690ed06e900cca2828dd30de
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-01 13:10:37 +0100

    Initial commit

---
+++ src/main/java/luqmanmohammad/U2D4SpringBootJpa/U2D4SpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D4SpringBootJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D4SpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D4SpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmanmohammad/U2D4SpringBootJpa/U2D4SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmanmohammad.U2D4SpringBootJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D4SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
